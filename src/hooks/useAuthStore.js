49449de91763c58b74e470a5b2a061d7336b443f
Jeral Pineda <jpineda@banhcafe.hn>
2023-05-09 09:04:25 -0600

    fix(authstore): Despachar accion del login

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -1,17 +1,19 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { calendarApi } from '../api';
-import { clearErrorMessage, onLogout } from '../store';
+import { clearErrorMessage, onChecking, onLogin, onLogout } from '../store';
 
 export const useAuthStore = () => {
   const { status, user, errorMessage } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
   const startLogin = async ({ email, password }) => {
-    console.log({ email, password });
+    dispatch(onChecking());
 
     try {
-      const resp = await calendarApi.post('/auth', { email, password });
-      console.log(resp?.data);
+      const { data } = await calendarApi.post('/auth', { email, password });
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('token-init-date', new Date().getTime());
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
     } catch (error) {
       // console.log(error);
       dispatch(onLogout('Credenciales incorrectas'));
