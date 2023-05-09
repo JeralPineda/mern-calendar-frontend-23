4a0f07e22975136a3e2762d1ae31e752fd1c4998
Jeral Pineda <jpineda@banhcafe.hn>
2023-05-09 11:57:47 -0600

    feat: Mantener el estado de la autenticacion

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -44,6 +44,21 @@ export const useAuthStore = () => {
     }
   };
 
+  const checkAuthToken = async () => {
+    const token = localStorage.getItem('token');
+    if (!token) return dispatch(onLogout('Token expiro'));
+
+    try {
+      const { data } = await calendarApi.get('auth/renew');
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('token-init-date', new Date().getTime());
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
+    } catch (error) {
+      localStorage.clear();
+      dispatch(onLogout());
+    }
+  };
+
   return {
     //* Propiedades
     status,
@@ -51,6 +66,7 @@ export const useAuthStore = () => {
     errorMessage,
 
     //* Métodos
+    checkAuthToken,
     startLogin,
     startRegister,
   };
